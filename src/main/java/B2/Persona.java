f6e11dc61eb250814d75015687f4c14cdb029201
Juan Manuel Ochoa <ochoajuanmanuel18@gmail.com>
2023-08-27 05:02:17 -0500

    Trabajo Bloque 2

---
+++ src/main/java/B2/Persona.java
@@ -0,0 +1,23 @@
+package B2;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private   String universidad;
+    private int semestre;
+    private int NumMaterias;
+
+    private String identificacion;
+
+    public Persona() {
+    }
+    public Persona(String nombre, int edad, String universidad,int semestre, int NumMaterias,String identificacion) {
+        this.edad = edad;
+        this.universidad = universidad;
+        this.semestre = semestre;
+        this.NumMaterias = NumMaterias;
+        this.identificacion = identificacion;
+    }}
+
+
+
---
+++ src/main/java/B2/TrabajoBloque2.java
@@ -0,0 +1,8 @@
+package B2;
+
+public class TrabajoBloque2 {
+    public static void main(String[] args) {
+
+        Persona persona = new Persona("Juanito Perez",17,"Icesi",4, 8, "1109865800");
+    }
+}
