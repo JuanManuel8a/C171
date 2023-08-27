e8b00bf2a05bb3fed59d039b79b3e1ebae2158e9
Juan Manuel Ochoa <ochoajuanmanuel18@gmail.com>
2023-08-27 03:37:57 -0500

    Tabajo Bloque 1

---
+++ src/main/java/B1/TrabajoBloque1.java
@@ -0,0 +1,28 @@
+package B1;
+
+public class TrabajoBloque1 {
+    public static void main(String[] args) {
+        int edad = 25;
+        double precio = 25/3;
+        char inicial = 'J';
+        boolean esVerdad = true;
+        String nombre = "Juan Manuel";
+        //Primitivos
+        short valores=-32768;
+        int num1=9; //primitivo
+        double num3=3.4;
+        char caracter='A';
+
+        //Wrapper
+        Short valor= 32767;
+        Integer num2 = 10; //Wrapper int
+        Double num10=3.4; //double
+        Character caraterW='T';
+        String text= "hola mundo"; //texto
+
+        System.out.println("" + text);
+
+
+
+    }
+}
